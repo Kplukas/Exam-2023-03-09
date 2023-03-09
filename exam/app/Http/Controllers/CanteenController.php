a2c7046686aed2c5234a3c113a1b4e14ddc509e7
Kristupas <114056612+Kplukas@users.noreply.github.com>
2023-03-09 08:27:48 +0200

    Models

---
+++ exam/app/Http/Controllers/CanteenController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Canteen;
+use App\Http\Requests\StoreCanteenRequest;
+use App\Http\Requests\UpdateCanteenRequest;
+
+class CanteenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCanteenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Canteen $canteen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Canteen $canteen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCanteenRequest $request, Canteen $canteen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Canteen $canteen)
+    {
+        //
+    }
+}
---
+++ exam/database/migrations/2023_03_09_061722_create_food_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('list_id');
+            $table->foreign('list_id')->references('id')->on('foodlists');
+            $table->string('title', 50);
+            $table->string('description', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('food');
+    }
+};
